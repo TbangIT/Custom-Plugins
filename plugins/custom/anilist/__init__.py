a07f387f97864068ade606336e2da6122de6a7fe
TbangIT <76477475+TbangIT@users.noreply.github.com>
2022-03-20 11:27:11 +0100

    Add files via upload

---
+++ plugins/custom/anilist/__init__.py
@@ -0,0 +1,8 @@
+# Module Capable of fetching Anime, Airing, Character Info &
+# Anime Reverse Search made for UserGe.
+# AniList Api (GitHub: https://github.com/AniList/ApiV2-GraphQL-Docs)
+# Anime Reverse Search Powered by tracemoepy.
+# TraceMoePy (GitHub: https://github.com/DragSama/tracemoepy)
+# (C) Author: Phyco-Ninja (https://github.com/Phyco-Ninja) (@PhycoNinja13b)
+
+""" Search for Anime related Info """
