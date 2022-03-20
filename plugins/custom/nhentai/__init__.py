d44b4b6e441c7f8e45d30be3a80fa4db085d0697
TbangIT <76477475+TbangIT@users.noreply.github.com>
2022-03-20 13:57:05 +0100

    Update __init__.py

--- plugins/custom/nhentai/__init__.py
+++ plugins/custom/nhentai/__init__.py
@@ -1,1 +1,5 @@
-
+# Copyright (C) 2020 KeselekPermen69
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
